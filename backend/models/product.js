c2be400d7543907e3409e2f2ca6bfb1e6c92d853
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-10 09:22:21 -0500

    Complete api to Product

---
+++ backend/models/product.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  code: Number,
+  description: String,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const product = mongoose.model("product", productSchema);
+module.exports = product;
